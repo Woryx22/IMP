97f2ba4af3fd381a4a7d0a5fb01830cdc19a8976
Lukáš Lešák <lukas.woryx@gmail.com>
2024-11-29 23:01:09 +0100

    1 nahrani, zaklady

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+import "./index.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header/>
+      <div className="bg-[url(./imgbg.png)] w-[100%] h-[100vh]  bg-cover bg-auto">
+        <h1 className="text-7xl font-bold underline text-center pt-[20%] text-white">KONÍČEK</h1>
+      </div>
+
+
     </div>
-  );
+    
+  )
 }
 
-export default App;
+
+export default App
