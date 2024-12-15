83d7e97a2e8e13829a003bfdbcd7aa9608b9491e
Lukáš Lešák <lukas.woryx@gmail.com>
2024-12-15 17:26:09 +0100

    Main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 import "./index.css"
 
 const App = () => {
   return (
     <div className="app">
-      <Header/>
-      <div className="bg-[url(./imgbg.png)] w-[100%] h-[100vh]  bg-cover bg-auto">
-        <h1 className="text-7xl font-bold underline text-center pt-[20%] text-white">KONÍČEK</h1>
-      </div>
+      <Header />
+      <Main/>
 
 
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function Header() {
     return (
-        <Disclosure as="nav" className="bg-gray-600 ">
+        <Disclosure as="nav" className="bg-gray-600 sticky top-0">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
 
@@ -65,7 +65,6 @@ export default function Header() {
                             type="button"
                             className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="absolute -inset-1.5" />
-                            <span className="sr-only">View notifications</span>
                             <UserCircleIcon aria-hidden="true" className="size-6" />
                         </button>
                     </div>
